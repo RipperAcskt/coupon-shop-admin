af75aa70c9dedace074910290a7fcff04e20890b
infinitss13 <stassanakoevBsuir@gmail.com>
2023-09-19 23:14:54 +0200

    added handlers for organization and members, inited router

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,59 @@
 package handlers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type AdminHandlers struct {
+}
+
+func NewAdminHandlers() *AdminHandlers {
+	return &AdminHandlers{}
+}
+
+func SetRequestHandlers() (*gin.Engine, error) {
+	router := gin.New()
+	organizationHandlers := NewAdminHandlers()
+	//router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	router.Use(CORSMiddleware())
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "hello message")
+	})
+	organization := router.Group("/organization")
+	{
+		organization.POST("/organization", organizationHandlers.createOrganization)
+		organization.GET("/organization", organizationHandlers.getOrganizations)
+		organization.DELETE("/organization", organizationHandlers.deleteOrganization)
+	}
+	members := organization.Group("/members")
+	{
+		members.POST("/members", organizationHandlers.addOrganizationMembers)
+		members.GET("/members", organizationHandlers.getOrganizationMembers)
+		members.DELETE("/members", organizationHandlers.deleteOrganizationMembers)
+
+	}
+	return router, nil
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		fmt.Println(c.Request.Method)
+
+		if c.Request.Method == "OPTIONS" {
+			fmt.Println("OPTIONS")
+			c.AbortWithStatus(200)
+		} else {
+			c.Next()
+		}
+	}
+}
