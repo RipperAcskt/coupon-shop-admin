d048fca7b4cb368a4eeda4f18043a7707a2b8dbd
RipperAcskt <7309333@gmail.com>
2023-09-30 16:16:51 +0300

    change CORS

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -86,7 +86,7 @@ func SetRequestHandlers(service Service, cfg config.Config) (*gin.Engine, error)
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
 		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
